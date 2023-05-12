c897e997d54e02fef5ffb97d31ea5788a1f69f77
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-05-13 04:56:01 +0500

    site header responsive finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-let a = 1;
+var elMenuBtn = document.querySelector(".site-header-burger");
+var elCloseBtn = document.querySelector(".close-button");
+var elMenuBurger = document.querySelector(".menu-burger");
+
+elMenuBtn.addEventListener("click", function(){
+  elMenuBurger.classList.add("menu-burger-active");
+})
+
+elCloseBtn.addEventListener("click", function(){
+  elMenuBurger.classList.remove("menu-burger-active");
+})
